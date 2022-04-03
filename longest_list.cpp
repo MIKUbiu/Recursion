669ee330d00f1e6e1dcc321c5beae1f60450fe49
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-04-03 18:36:41 +0800

    Add files via upload

---
+++ longest_list.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+using ll = long long;
+const int maxn = 1E6;
+int dp[maxn];
+int a[1000+10];
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		dp[0] = a[0];
+		for (int i = 0; i < n; i++) {
+			dp[i] = max(dp[i], dp[i - 1] + a[i]);//在前一项的最大和和本身中选一个
+		}
+		cout << *max_element(dp, dp + n) << endl;
+	}
+	return 0;
+}
